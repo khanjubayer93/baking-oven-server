26afc7fe708b284bb68a3623cd36ea1232328b7f
khanjubayer93 <jubayerkhan711@gmail.com>
2023-02-20 11:12:10 +0600

    jwt api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ObjectId } = require("mongodb")
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -14,6 +15,23 @@ app.use(express.json());
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRETE_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+    // console.log(authHeader);
+
+}
+
 async function conndeDB() {
     try {
 
@@ -22,6 +40,13 @@ async function conndeDB() {
         const reviewCollection = client.db('bakingOven').collection('reviews');
         const orderCollection = client.db('bakingOven').collection('orders');
 
+
+        // jwt api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JWT_SECRETE_TOKEN, { expiresIn: '1d' })
+            res.send({ token })
+        });
 
         // users api
         app.post('/users', async (req, res) => {
@@ -89,11 +114,36 @@ async function conndeDB() {
 
 
         // orders api
-        app.post('/services', async (req, res) => {
+        app.post('/orders', async (req, res) => {
             const products = req.body;
             const result = await orderCollection.insertOne(products);
             res.send(result)
         });
+
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+            console.log('inside api', decoded);
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const order = await cursor.toArray();
+            res.send(order)
+        });
+
+        // delete api
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const deleteItem = await orderCollection.deleteOne(query);
+            res.send(deleteItem)
+        })
 
     }
     finally {
